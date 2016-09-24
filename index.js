1fb5db2d927874c99ea0b7a7157f93047b23fc83
goofatgit <tl@tlhome.de>
2016-09-24 15:07:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Service, Characteristic;
 var s7client = new snap7.S7Client();
 
 module.exports = function(homebridge) {
-  console.log("homebridge API version: " + homebridge.version);
+//  console.log("homebridge API version: " + homebridge.version);
 
   // Accessory must be created from PlatformAccessory Constructor
 //  Accessory = homebridge.platformAccessory;
@@ -22,28 +22,58 @@ module.exports = function(homebridge) {
 function S7PLCAccessory(log, config) {
     this.log = log;
     this.name = config['name'];
+    this.bulbName = config["bulb_name"] || this.name;
+    this.binaryState = 0;
 //    this.db = config['db'];
-    this.service = new Service.Lightbulb(this.name);
+ 
 
 //    if (!this.db) throw new Error('You must provide a config value for db.');
 
 }
 S7PLCAccessory.prototype = setPowerOn: function(powerOn,callback) {
    
-    s7client.ConnectTo('192.168.1.240', 0, 2, function(err) {
+    s7client.ConnectTo('192.168.1.240', 0, 2, function(err)) {
       if(err)
         return console.log(' >> Connection failed. Code #' + err + ' - ' + s7client.ErrorText(err));
         
         // Read the first byte from PLC process outputs...
-      s7client.DBWrite(20, 0.0, 1, powerOn, function(err, res) {
+      s7client.DBWrite(20, 0.0, 1, powerOn, function(err, res)) {
         if(err)
           return console.log(' >> ABRead failed. Code #' + err + ' - ' + s7client.ErrorText(err));
+          
+          // ... and write it to stdout
+        console.log(res)
         
-        // ... and write it to stdout
-      console.log(res)
-      
-      S7Client.Disconnect()
-      callback(null);
-    });
-  });
+        S7Client.Disconnect()
+        callback(null);
+      };
+    };
+  };
+  S7PLCAccessory.prototype = getPowerOn: function(callback) {
+     
+    s7client.ConnectTo('192.168.1.240', 0, 2, function(err)) {
+      if(err)
+        return console.log(' >> Connection failed. Code #' + err + ' - ' + s7client.ErrorText(err));
+        
+        // Read the first byte from PLC process outputs...
+      s7client.DBRead(20, 0.0, 1, function(err, res)) {
+        if(err)
+          return console.log(' >> DBRead failed. Code #' + err + ' - ' + s7client.ErrorText(err));
+          
+          // ... and write it to Console and output
+        return console.log(res);
+        S7Client.Disconnect()
+        callback(null, res);
+       };
+      };
+  };
+    S7PLCAccessory.prototype.getServices = function() {
+    var lightbulbService = new Service.Lightbulb(this.name);
+    
+    lightbulbService
+      .getCharacteristic(Characteristic.On)
+      .on('get', this.getPowerOn.bind(this))
+      .on('set', this.setPowerOn.bind(this));
+    
+    return [lightbulbService];
 }
