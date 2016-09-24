cda958d8bae065d98cda44a48c0be9d6a02c4cd9
goofatgit <tl@tlhome.de>
2016-09-24 20:05:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ S7PLCAccessory.prototype.getPowerOn = function(callback) {
         
         // Read the first byte from PLC process outputs...
       s7client.DBRead(20, 0, 1, function(err, result) {
-        console.log("DBRead result is: %s", result);
+        console.log("DBRead result is: " + result);
         S7PLCAccessory.state = result;
         if(err)
           return console.log(' >> DBRead failed. Code #' + err + ' - ' + s7client.ErrorText(err));
